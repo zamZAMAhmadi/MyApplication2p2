f05084687d31f7f6dd85d0f54fc83051a65fa07a
Zamzama Ahmadi <zamzama2007@gmail.com>
2023-08-29 10:15:10 -0700

    Test

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,19 +3,39 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button incrementButton;
     TextView greetingDisplay;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TextView textview = new TextView(this);
-        //textview.setText(R.string.hello_dad);
         setContentView(R.layout.activity_main);
+        incrementButton = findViewById(R.id.increment_button);
+        greetingDisplay = findViewById(R.id.greeting_textview);
+        greetingDisplay = findViewById(R.id.greeting_textview);
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("incrementing: " + ++count);
+                Log.i("incrementing", "" + count);
+                greetingDisplay.setText("" + count);
+
+            }
+        });
+
+
         //setContentView(textview);
 
+    }
+
+    public void decrement(View view) {
+        System.out.println("decrementing: " + --count);
+        greetingDisplay.setText(""+ count);
     }
 }
